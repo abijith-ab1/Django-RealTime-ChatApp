d27e02e5ae5b36c33fe4ba62e6b969f42e92876c
abijith-ab1 <abijithkp93@gmail.com>
2024-04-11 12:18:39 +0530

    setup redis database

--- a_core/settings.py
+++ a_core/settings.py
@@ -87,12 +87,23 @@ TEMPLATES = [
     },
 ]
 
+
 # WSGI_APPLICATION = 'a_core.wsgi.application'
 ASGI_APPLICATION = "a_core.asgi.application"
+
+# CHANNEL_LAYERS = {
+#     'default': {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     }
+# }
+
 CHANNEL_LAYERS = {
-    'default': {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(env('REDIS'))],
+        },
+    },
 }
 
 
